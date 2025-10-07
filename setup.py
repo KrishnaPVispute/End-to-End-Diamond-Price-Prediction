235ee82846cd1e0e5063f4db6b86fe3e0cf5f114
KrishnaVispute <krishnavispute2004@gmail.com>
2025-10-07 10:46:24 +0530

    Initial Hugging Face upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages,setup
 from typing import List
 hypen_e_dot = '-e.'
-def get_requirements(file_path:str)->List[str]:
-    requirements =[]
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n',"")for req in requirements]
-        if hypen_e_dot in requirements:
-            requirements.remove(hypen_e_dot)
-        return requirements
+    def get_requirements(file_path:str)->List[str]:
+        requirements =[]
+        with open(file_path) as file_obj:
+            requirements = file_obj.readlines()
+            requirements = [req.replace('\n',"")for req in requirements]
+            if hypen_e_dot in requirements:
+                requirements.remove(hypen_e_dot)
+            return requirements
 
 setup(
     name ="DiamondPricePrediction",
